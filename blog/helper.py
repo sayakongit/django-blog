06e9f510e2448c56ce5022f2654c4691366cc6b9
Sayak Saha <83216382+sayakongit@users.noreply.github.com>
2023-05-13 22:35:33 +0530

    mail settings changed

--- blog/helper.py
+++ blog/helper.py
@@ -1,10 +1,12 @@
 from django.core.mail import send_mail
 from django.conf import settings
 
+URL = 'https://web-production-d9c5.up.railway.app'
+
 
 def send_forget_password_mail(email, token):
-    subject = 'Your forget password link'
-    message = f'Hi , click on the link to reset your password http://127.0.0.1:8000/change_password/{token}/'
+    subject = '[Action Required] News & Blogs | Password Reset Mail'
+    message = f'Hi , click on the link to reset your password {URL}/change_password/{token}/'
     email_from = settings.EMAIL_HOST_USER
     recipient_list = [email]
     send_mail(subject, message, email_from, recipient_list)
